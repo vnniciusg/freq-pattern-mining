3169b992cc8fde8e585da3e7d13f6f80676e54d3
vnniciusg <vinnicius109@gmail.com>
2025-07-27 12:48:32 -0300

    feat: implement data loading functionality from Kaggle dataset

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+import warnings
+
+import pandas as pd
+from kagglehub import dataset_download
+
+warnings.filterwarnings("ignore")
+
+def load_df(handle: str = "rukenmissonnier/real-market-data") -> pd.DataFrame:
+
+    path = dataset_download(handle)
+    files = os.listdir(path)
+
+    for file in files:
+        if file.endswith(".csv"):
+            return pd.read_csv(os.path.join(path, file), sep=";").astype(bool)
+
 def main():
-    print("Hello from freq-pattern-mining!")
+    df = load_df()
+    print(df.head())
 
 
 if __name__ == "__main__":
